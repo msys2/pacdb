449ef63ebffef8ff17e6876d0a7f4dd57daff23c
Jeremy Drake <github@jdrake.com>
2025-02-07 10:52:18 -0800

    examples/msys32.py: add hack for msys2-runtime-3.3
    it was renamed to msys2-runtime for i686 in msys2/MSYS2-packages#5188

--- examples/msys32.py
+++ examples/msys32.py
@@ -8,6 +8,9 @@ msys64 = pacdb.msys_db_by_arch('x86_64')
 pkgs64 = {str(pkg): pkg for pkg in msys64}
 pkgs32 = {str(pkg): pkg for pkg in build32}
 
+# HACK for rename of msys2-runtime-3.3 to msys2-runtime on i686
+pkgs32.update({k[:13] + "-3.3" + k[13:]: v for k, v in pkgs32.items() if v.name in ("msys2-runtime", "msys2-runtime-devel")})
+
 updates = pkgs64.keys() - pkgs32.keys()
 
 def base(pkg):
