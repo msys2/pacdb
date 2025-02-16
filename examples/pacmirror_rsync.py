a29c9d1e08d2e69a8f9b2cac2a2715cb653a3721
Jeremy Drake <github@jdrake.com>
2025-02-15 22:56:28 -0800

    examples: add no-sync-db option to pacmirror_rsync
    This will be useful to sync the files referenced in an old db.

--- examples/pacmirror_rsync.py
+++ examples/pacmirror_rsync.py
@@ -8,6 +8,7 @@ import sys
 
 parser = argparse.ArgumentParser(description='Mirror packages from a pacman sync db, via rsync')
 parser.add_argument('-e', '--repo', required=True, help='pacman repo name to mirror')
+parser.add_argument('-n', '--no-sync-db', action='store_true')
 parser.add_argument('-v', '--verbose', action='count', help='output additional information')
 parser.add_argument('url', help='source rsync url')
 parser.add_argument('dir', help='destination dir')
@@ -18,17 +19,20 @@ def popen_rsync():
     return subprocess.Popen(["rsync", "-rptHL" + ("v" if options.verbose else ""), "--from0", "--files-from", "-", options.url, options.dir], stdin=subprocess.PIPE, close_fds=True, text=True)
 
 
-files = set()
 # unfortunately, I don't think there's any way to know the .tar.X extension,
 # so just get the straight .db and .files without the extra extensions
-p = popen_rsync()
-for t in (".db", ".files"):
-    files.add(options.repo + t)
-    print(options.repo + t, end="\0", file=p.stdin)
-    files.add(options.repo + t + ".sig")
-    print(options.repo + t + ".sig", end="\0", file=p.stdin)
-p.stdin.close()
-p.wait()
+if options.no_sync_db:
+    files = set(options.repo + ext + sig for sig in ("", ".sig") for ext in (".db", ".files"))
+else:
+    files = set()
+    p = popen_rsync()
+    for t in (".db", ".files"):
+        files.add(options.repo + t)
+        print(options.repo + t, end="\0", file=p.stdin)
+        files.add(options.repo + t + ".sig")
+        print(options.repo + t + ".sig", end="\0", file=p.stdin)
+    p.stdin.close()
+    p.wait()
 
 db = pacdb.Database(options.repo, filename=os.path.join(options.dir, options.repo+".db"))
 
